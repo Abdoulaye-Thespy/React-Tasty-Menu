f222b7b5a3eef72b46d17805774e2b3ee988c121
taiwocoker <taiwofcoker@gmail.com>
2020-10-22 10:13:54 +0100

    Refactored actions to search meals by letters

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,41 +1,48 @@
-import axios from 'axios'
-import { GET_MENUS, GET_MENU, CHANGE_FILTER } from '../constants/actionTypes'
+import axios from 'axios';
+import letters from './allLetters';
+import { GET_MENUS, GET_MENU, CHANGE_FILTER, LOADING, SEARCH_BY_LETTER } from '../constants/actionTypes'
 
 // const apiUrl = 'https://www.themealdb.com/api/json/v1/1/categories.php'
-export const getMenus = () => async (dispatch) => {
-  try {
-    const allMenu = await axios.get(
-      'https://www.themealdb.com/api/json/v1/1/categories.php'
-    )
+export const getMenus = () => (dispatch) => {
+  const lettersResult = [];
 
-    dispatch({
-      type: GET_MENUS,
-      payload: allMenu.data.categories,
-    })
-  } catch (err) {
-    dispatch({
-      type: GET_MENUS,
-      payload: [],
-    })
-  }
+  letters.map((letter) => {
+    axios
+      .get(`https://www.themealdb.com/api/json/v1/1/search.php?f=${letter}`)
+      .then((res) => {
+        lettersResult.push(...res.data.meals);
+      })
+      .then(() => {
+        dispatch({
+          type: GET_MENUS,
+          payload: lettersResult,
+        })
+      })
+  })
 }
 
-export const getMenu = (id) => async (dispatch) => {
-  try {
-    const res = await axios.get(
-      `https://www.themealdb.com/api/json/v1/1/categories.php/${id}`
-    )
-    dispatch({
-      type: GET_MENU,
-      payload: res.data.categories,
-    })
-  } catch (err) {
-    dispatch({
-      type: GET_MENU,
-      payload: [],
-    })
-  }
+export const searchMenu = (letter) => async (dispatch) => {
+  const data = await axios.get(
+    `https://www.themealdb.com/api/json/v1/1/search.php?f=${letter}`
+  )
+  dispatch({
+    type: SEARCH_BY_LETTER,
+    payload: data,
+  })
 }
+
+export const getMenu = (id) => (dispatch) => {
+  axios
+  .get(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`)
+  .then(response => dispatch({
+    type: GET_MENU,
+    payload: response.data.meals[0],
+  }));
+};
+
+export  const setLoading = () => ({
+  type: LOADING,
+})
 
 export const changeFilter = (filter) => ({
   type: CHANGE_FILTER,
--- src/constants/actionTypes.js
+++ src/constants/actionTypes.js
@@ -1,3 +1,5 @@
-export const GET_MENUS = 'GET_MENUS'
-export const GET_MENU = 'GET_MENU'
-export const CHANGE_FILTER = 'CHANGE_FILTER'
+export const GET_MENUS = 'GET_MENUS';
+export const GET_MENU = 'GET_MENU';
+export const CHANGE_FILTER = 'CHANGE_FILTER';
+export const SEARCH_BY_LETTER = 'SEARCH_BY_LETTER';
+export const LOADING = 'LOADING';
